4d793d20db4ce547d4b95eb36bb0233fe270b206
nrbjerg <mart547n@outlook.dk>
2022-04-28 14:18:42 +0000

    Lavede vores kode om til R

--- code/poisson.R
+++ code/poisson.R
@@ -1,7 +1,7 @@
 library(purrr)
 library(eList)
 library(plyr)
-# library(tidyverse)
+library(ggplot2)
 
 poisson_pmf <- function(k) {
   # Here lambda = 1
@@ -24,24 +24,56 @@ simulate_variable <- function(u, pmf = poisson_pmf) {
 
 simulate_poisson_pmf <- function(n) {
   # Simulates a sequence and creates a barplot
+  set.seed(2) # makes sure to that the first uniform random variables are always the same
   u <- runif(n = n)
   simulated_frequencies <- table(unlist(map(u, simulate_variable))) / n
   return(simulated_frequencies)
 }
 
-ns <- c(100, 1000, 5000) 
-results <- List(for(n in ns) simulate_poisson_pmf(n))
+transform_dataset <- function(df, row_ids) {
+  dfs <- List()
+  for (i in seq(row_ids)) {
+    row_id <- row_ids[i]
+    row <- df[row_id, ]
+    print(ncol(row))
+    typ <- rep(c(row_id), each = ncol(df))
+    k <- 0:(ncol(df) - 1)
+    # Convert p(k) to column vector
+    col <- t(row)
+    colnames(col) <- c("p.k")
+    dfs[[i]] <- data.frame(k = k, simulation.type = typ, p.k = col)
+  }
+  return(as.data.frame(do.call(rbind, dfs)))
+}
+
+ns <- c(1000, 2000, 5000)
+results <- List(for (n in ns) simulate_poisson_pmf(n))
 
 # Append zeroes to the back of the results vectors & create dataframe.
 max.length <- max(sapply(results, length))
-results <- lapply(results, function(v) {c(v, rep(0, max.length - length(v)))})
+results <- lapply(results, function(v) {
+  c(v, rep(0, max.length - length(v)))
+})
 df <- as.data.frame(do.call(rbind, results))
-rownames(df) <- ns # Make the ns the rownames of the dataframe
+# Add true pmd, aswell as column and row names
+print(max.length)
+max.k <- max.length - 1
+ks <- c(List(for (k in 0:max.k) k))
+names(df) <- ks
+
+true.pmf <- data.frame(List(for (k in ks) poisson_pmf(k)))
+
+names(true.pmf) <- ks
+df <- rbind(df, true.pmf)[, c(1, 2, 3, 4)] # Only get the first 4 columns
+row_ids <- append(ns, c("true pmf"))
+rownames(df) <- row_ids
+# Transform dataframe for plotting
+df <- transform_dataset(df, row_ids)
 print(df)
-# Convert to table of values & their frequency
-# X11()
-# barplot(simulated_frequencies,
-#  main = "Simulations of poisson distrobution",
-#  xlab = "k",
-#  ylab = "simulated p(k)",
-# )
+
+theme_set(
+  theme(legend.position = "top")
+)
+
+ggplot(df, aes(x = k, y = p.k, fill = simulation.type)) +
+  geom_col(position = "dodge")
